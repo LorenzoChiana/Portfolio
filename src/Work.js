b7d851cc1abf3cc2a93fd19a981a13155f8c830d
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-10-29 10:39:03 +0100

    Implemented work page sample

--- src/Work.js
+++ src/Work.js
@@ -1,14 +1,51 @@
 import React, { Component } from "react";
+import "./Work.css";
+import portfolio from "./img/portfolio.png";
+
+ 
 class Work extends Component {
   render() {
     return (
-      <div>
-        <h2>Work</h2>
-        <div className="flex-container">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div> 
+      <div className="work">
+        <h2>Projects</h2>
+          <div className="container">
+            <a className="card" href="https://github.com/LorenzoChiana/portfolio" target="_blank">
+              <img src={portfolio} alt="project-image" />
+              <div className="txt-container">
+                <h4>
+                  <b>Portfolio </b>
+                  <i>( current )</i>
+                  <br />
+                  Portfolio Website
+                </h4> 
+                <p>2019 <span className="slashes">//</span> React</p> 
+              </div>
+            </a> 
+
+            <a className="card" href="https://github.com/LorenzoChiana/BikeSharing" target="_blank">
+              <img src="https://u.imageresize.org/v2/487daa4a-5f87-435f-bc28-c040f1897fbe.png" alt="project-image" />
+              <div className="txt-container">
+                <h4><b>BikeSharing</b><br />Applicazioni e Servizi Web Website</h4> 
+                <p>2017 <span className="slashes">//</span> Angular / Node / SCSS / MongoDB</p> 
+              </div>
+            </a>
+            
+            <a className="card" href="https://github.com/LorenzoChiana/Vakcino" target="_blank">
+              <img src="https://u.imageresize.org/v2/487daa4a-5f87-435f-bc28-c040f1897fbe.png" alt="project-image" />
+              <div className="txt-container">
+                <h4><b>Vakcino</b><br />Andorid App</h4> 
+                <p>2016 <span className="slashes">//</span> Android / Java / XML</p> 
+              </div>
+            </a>
         </div>
+
+
+           
+        <h2>boh</h2>
+        <div className="container">
+        </div>
+
+        
       </div>
     );
   }
