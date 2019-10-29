aab3f21940033a373bf6a39439ff118de68d3df2
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-10-29 13:39:01 +0100

    Sample home structure and style

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
+import "./Home.css";
+import * as Icon from 'react-feather';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Home</h2>
-        <div className="flex-container">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div> 
+      <div className="home">
+        <div className="container"></div>
+       
+        <div className="container work-section">
+          <a href="#/work">
+            <div className="container-item one">
+              <h2>Check out some of my previous work</h2>
+              <Icon.ArrowRightCircle />
+            </div>
+          </a>
+          <a href="#/about">
+            <div className="container-item two">
+              <h2>About me</h2>
+              <Icon.ArrowRightCircle />
+            </div>
+          </a>
         </div>
       </div>
     );
