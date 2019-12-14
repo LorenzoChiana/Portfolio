d8777380ea19d83ac8742f935c19bfd01b16f11a
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2019-12-14 17:57:31 +0100

    Added new imgs on works

--- src/Work.js
+++ src/Work.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import "./Work.css";
 import github from "./img/github.png";
+import bikesharing from "./img/bikesharing.jpg";
+import vaccine from "./img/vaccine.jpg";
+import filemanager from "./img/filemanager.jpg";
+import sol from "./img/exams.jpg";
+import portfolio from "./img/portfolio.jpg";
 import * as Icon from 'react-feather';
 
  
@@ -11,7 +16,7 @@ class Work extends Component {
         <h2>Projects</h2>
           <div className="container">
             <a className="card" href="https://github.com/LorenzoChiana/portfolio" target="_blank">
-              <img src={github} alt="project-image" />
+              <img src={portfolio} alt="project-image" />
               <div className="txt-container">
                 <h4>
                   <b>Portfolio </b>
@@ -27,7 +32,7 @@ class Work extends Component {
             <div className="container">
 
             <a className="card" href="https://github.com/LorenzoChiana/BikeSharing" target="_blank">
-              <img src={github} alt="project-image" />
+              <img src={bikesharing} alt="project-image" />
               <div className="txt-container">
                 <h4><b>BikeSharing</b><br />Website for the management of a hypothetical bike-sharing service. Developed on a MEAN stack.</h4> 
                 <p>2017 <span className="slashes">//</span> Angular / Node / SCSS / MongoDB</p> 
@@ -35,7 +40,7 @@ class Work extends Component {
             </a>
             
             <a className="card" href="https://github.com/LorenzoChiana/Vakcino" target="_blank">
-              <img src={github} alt="project-image" />
+              <img src={vaccine} alt="project-image" />
               <div className="txt-container">
                 <h4><b>Vakcino</b><br />Android application that allows the user to manage the vaccination record both for himself and for other family members.</h4> 
                 <p>2016 <span className="slashes">//</span> Android / Java / XML / SQL / MVC </p> 
@@ -43,7 +48,7 @@ class Work extends Component {
             </a>
 
             <a className="card" href="https://github.com/LorenzoChiana/sol-project" target="_blank">
-              <img src={github} alt="project-image" />
+              <img src={sol} alt="project-image" />
               <div className="txt-container">
                 <h4><b>SOL restyle</b><br />Recreation of the home page of SOL platform (Studenti Online) of the University of Bologna to which was added the functionality of sharing notes and slides between students and professors.</h4> 
                 <p>2015 <span className="slashes">//</span> HTML / PHP / JavaScript / CSS / SQL</p> 
@@ -51,7 +56,7 @@ class Work extends Component {
             </a>
 
             <a className="card" href="https://github.com/LorenzoChiana/FileMiner" target="_blank">
-              <img src={github} alt="project-image" />
+              <img src={filemanager} alt="project-image" />
               <div className="txt-container">
                 <h4><b>FileMiner</b><br />Creation of a file manager in Java with MVC pattern for the course of OOP (Object Oriented Programming).</h4> 
                 <p>2015 <span className="slashes">//</span> Java / MVC</p> 
